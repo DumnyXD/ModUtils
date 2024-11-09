9eeef23024a0f606335053080b7c338eb44e903c
WallyssonSG <129697090+DumnyXD@users.noreply.github.com>
2024-11-09 15:29:57 -0300

    Add creative mod tabs para blocos e items do mod

--- src/main/java/net/dumny/modutils/ModUtils.java
+++ src/main/java/net/dumny/modutils/ModUtils.java
@@ -1,6 +1,7 @@
 package net.dumny.modutils;
 
 import net.dumny.modutils.block.ModBlocks;
+import net.dumny.modutils.item.ModCreativeModTabs;
 import net.dumny.modutils.item.ModItems;
 import net.minecraft.world.item.CreativeModeTabs;
 import org.slf4j.Logger;
@@ -36,6 +37,8 @@ public class ModUtils {
         // Note that this is necessary if and only if we want *this* class (ExampleMod) to respond directly to events.
         // Do not add this line if there are no @SubscribeEvent-annotated functions in this class, like onServerStarting() below.
         NeoForge.EVENT_BUS.register(this);
+
+        ModCreativeModTabs.register(modEventBus);
 
         ModItems.register(modEventBus);
         ModBlocks.register(modEventBus);
---
+++ src/main/java/net/dumny/modutils/item/ModCreativeModTabs.java
@@ -0,0 +1,42 @@
+package net.dumny.modutils.item;
+
+import net.dumny.modutils.ModUtils;
+import net.dumny.modutils.block.ModBlocks;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.network.chat.Component;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.ItemStack;
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.neoforge.registries.DeferredRegister;
+
+import java.util.function.Supplier;
+
+public class ModCreativeModTabs {
+    public static final DeferredRegister<CreativeModeTab> CREATIVE_MODE_TAB =
+            DeferredRegister.create(Registries.CREATIVE_MODE_TAB, ModUtils.MOD_ID);
+
+    public static final Supplier<CreativeModeTab> BISMUTH_ITEMS_TAB = CREATIVE_MODE_TAB.register("bismuth_items_tab",
+            () -> CreativeModeTab.builder()
+                    .icon(() -> new ItemStack(ModItems.BISMUTH.get()))
+                    .title(Component.translatable("creativetab.modutils.bismuth_items"))
+                    .displayItems((itemDisplayParameters, output) -> {
+                        output.accept(new ItemStack(ModItems.BISMUTH.get()));
+                        output.accept(new ItemStack(ModItems.RAW_BISMUTH.get()));
+                    })
+                    .build());
+    public static final Supplier<CreativeModeTab> BISMUTH_BLOCKS_TAB = CREATIVE_MODE_TAB.register("bismuth_blocks_tab",
+            () -> CreativeModeTab.builder()
+                    .withTabsBefore(ResourceLocation.fromNamespaceAndPath(ModUtils.MOD_ID, "bismuth_items_tab"))
+                    .icon(() -> new ItemStack(ModBlocks.BISMUTH_BLOCK.get()))
+                    .title(Component.translatable("creativetab.modutils.bismuth_blocks"))
+                    .displayItems((itemDisplayParameters, output) -> {
+                        output.accept(new ItemStack(ModBlocks.BISMUTH_BLOCK.get()));
+                        output.accept(new ItemStack(ModBlocks.BISMUTH_ORE.get()));
+                    })
+                    .build());
+
+    public static void register(IEventBus eventBus) {
+        CREATIVE_MODE_TAB.register(eventBus);
+    }
+}
