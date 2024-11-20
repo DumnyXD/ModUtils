c0f4d1bdfb1a9410b85ac1359ecbe6a21849fd06
WallyssonSG <129697090+DumnyXD@users.noreply.github.com>
2024-11-20 14:05:19 -0300

    Criando tooltip em alguns itens

--- src/main/java/net/dumny/modutils/item/custom/ChiselItem.java
+++ src/main/java/net/dumny/modutils/item/custom/ChiselItem.java
@@ -1,17 +1,22 @@
 package net.dumny.modutils.item.custom;
 
 import net.dumny.modutils.block.ModBlocks;
+import net.minecraft.client.gui.screens.Screen;
+import net.minecraft.network.chat.Component;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.sounds.SoundEvents;
 import net.minecraft.sounds.SoundSource;
 import net.minecraft.world.InteractionResult;
 import net.minecraft.world.entity.EquipmentSlot;
 import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.TooltipFlag;
 import net.minecraft.world.item.context.UseOnContext;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
 
+import java.util.List;
 import java.util.Map;
 
 public class ChiselItem extends Item {
@@ -44,7 +49,17 @@ public class ChiselItem extends Item {
                 level.playSound(null, context.getClickedPos(), SoundEvents.GRINDSTONE_USE, SoundSource.BLOCKS);
             }
         }
-
         return InteractionResult.SUCCESS;
+    }
+
+    @Override
+    public void appendHoverText(ItemStack stack, TooltipContext context, List<Component> tooltipComponents, TooltipFlag tooltipFlag) {
+        if(Screen.hasShiftDown()){
+            tooltipComponents.add(Component.translatable("tooltip.modutils.chisel.shift_down"));
+        }else{
+            tooltipComponents.add(Component.translatable("tooltip.modutils.chisel"));
+        }
+
+        super.appendHoverText(stack, context, tooltipComponents, tooltipFlag);
     }
 }
