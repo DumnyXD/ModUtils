f362816f76c09c23f181ae33b4359f3dee9ee165
WallyssonSG <129697090+DumnyXD@users.noreply.github.com>
2024-11-20 22:08:53 -0300

    ModRecipeProvider feito e funcionando

--- src/main/java/net/dumny/modutils/datagen/ModRecipeProvider.java
+++ src/main/java/net/dumny/modutils/datagen/ModRecipeProvider.java
@@ -1,4 +1,46 @@
 package net.dumny.modutils.datagen;
 
-public class ModRecipeProvider {
+import net.dumny.modutils.block.ModBlocks;
+import net.dumny.modutils.item.ModItems;
+import net.minecraft.core.HolderLookup;
+import net.minecraft.data.PackOutput;
+import net.minecraft.data.recipes.*;
+import net.minecraft.world.level.ItemLike;
+
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
+
+public class ModRecipeProvider extends RecipeProvider {
+    public ModRecipeProvider(PackOutput output, CompletableFuture<HolderLookup.Provider> registries) {
+        super(output, registries);
+    }
+
+    @Override
+    protected void buildRecipes(RecipeOutput recipeOutput) {
+        List<ItemLike> BISMUTH_SMELTABLES = List.of(
+                ModItems.RAW_BISMUTH.get(),
+                ModBlocks.BISMUTH_ORE.get(),
+                ModBlocks.BISMUTH_DEEPSLATE_ORE.get()
+        );
+
+        ShapedRecipeBuilder.shaped(RecipeCategory.MISC, ModBlocks.BISMUTH_BLOCK.get())
+                .pattern("BBB")
+                .pattern("BBB")
+                .pattern("BBB")
+                .define('B', ModItems.BISMUTH.get())
+                .unlockedBy("has_bismuth", has(ModItems.BISMUTH.get()))
+                .save(recipeOutput);
+
+        ShapelessRecipeBuilder.shapeless(RecipeCategory.MISC, ModItems.BISMUTH.get(),9)
+                .requires(ModBlocks.BISMUTH_BLOCK.get())
+                .unlockedBy("has_bismuth_block", has(ModBlocks.BISMUTH_BLOCK.get()))
+                .save(recipeOutput);
+        ShapelessRecipeBuilder.shapeless(RecipeCategory.MISC, ModItems.BISMUTH.get(),18)
+                .requires(ModBlocks.MAGIC_BLOCK.get())
+                .unlockedBy("has_magic_block", has(ModBlocks.MAGIC_BLOCK.get()))
+                .save(recipeOutput, "modutils:bismuth_from_magic_block");
+
+        oreSmelting(recipeOutput, BISMUTH_SMELTABLES,RecipeCategory.MISC, ModItems.BISMUTH.get(), 0.7f, 200, "bismuth");
+        oreBlasting(recipeOutput, BISMUTH_SMELTABLES,RecipeCategory.MISC, ModItems.BISMUTH.get(), 0.7f, 100, "bismuth");
+    }
 }
