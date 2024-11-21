b1faa9e524e54a20e432a34e3ee56ea4338711ac
WallyssonSG <129697090+DumnyXD@users.noreply.github.com>
2024-11-20 21:52:33 -0300

    ModItemTagProvider e ModBlockTagProvider feitos e funcionando

--- src/main/java/net/dumny/modutils/datagen/ModBlockTagProvider.java
+++ src/main/java/net/dumny/modutils/datagen/ModBlockTagProvider.java
@@ -1,4 +1,35 @@
 package net.dumny.modutils.datagen;
 
-public class ModBlockTagProvider {
+import net.dumny.modutils.ModUtils;
+import net.dumny.modutils.block.ModBlocks;
+import net.minecraft.core.HolderLookup;
+import net.minecraft.data.PackOutput;
+import net.minecraft.tags.BlockTags;
+import net.neoforged.neoforge.common.data.BlockTagsProvider;
+import net.neoforged.neoforge.common.data.ExistingFileHelper;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.concurrent.CompletableFuture;
+
+public class ModBlockTagProvider extends BlockTagsProvider {
+    public ModBlockTagProvider(PackOutput output, CompletableFuture<HolderLookup.Provider> lookupProvider, @Nullable ExistingFileHelper existingFileHelper) {
+        super(output, lookupProvider, ModUtils.MOD_ID, existingFileHelper);
+    }
+
+    @Override
+    protected void addTags(HolderLookup.Provider provider) {
+        tag(BlockTags.MINEABLE_WITH_PICKAXE)
+                .add(ModBlocks.BISMUTH_BLOCK.get())
+                .add(ModBlocks.BISMUTH_ORE.get())
+                .add(ModBlocks.BISMUTH_DEEPSLATE_ORE.get())
+                .add(ModBlocks.MAGIC_BLOCK.get());
+
+        tag(BlockTags.NEEDS_IRON_TOOL)
+                .add(ModBlocks.BISMUTH_BLOCK.get())
+                .add(ModBlocks.BISMUTH_ORE.get())
+                .add(ModBlocks.BISMUTH_DEEPSLATE_ORE.get());
+
+        tag(BlockTags.NEEDS_DIAMOND_TOOL)
+                .add(ModBlocks.MAGIC_BLOCK.get());
+    }
 }
--- src/main/java/net/dumny/modutils/datagen/ModItemTagProvider.java
+++ src/main/java/net/dumny/modutils/datagen/ModItemTagProvider.java
@@ -1,4 +1,30 @@
 package net.dumny.modutils.datagen;
 
-public class ModItemTagProvider {
+import net.dumny.modutils.ModUtils;
+import net.dumny.modutils.item.ModItems;
+import net.dumny.modutils.util.ModTags;
+import net.minecraft.core.HolderLookup;
+import net.minecraft.data.PackOutput;
+import net.minecraft.data.tags.ItemTagsProvider;
+import net.minecraft.world.item.Items;
+import net.minecraft.world.level.block.Block;
+import net.neoforged.neoforge.common.data.ExistingFileHelper;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.concurrent.CompletableFuture;
+
+public class ModItemTagProvider extends ItemTagsProvider {
+    public ModItemTagProvider(PackOutput output, CompletableFuture<HolderLookup.Provider> lookupProvider, CompletableFuture<TagLookup<Block>> blockTags, @Nullable ExistingFileHelper existingFileHelper) {
+        super(output, lookupProvider, blockTags, ModUtils.MOD_ID, existingFileHelper);
+    }
+
+    @Override
+    protected void addTags(HolderLookup.Provider provider) {
+        tag(ModTags.Items.TRANSFORMABLE_ITEMS)
+                .add(ModItems.BISMUTH.get())
+                .add(ModItems.RAW_BISMUTH.get())
+                .add(Items.COAL)
+                .add(Items.STICK)
+                .add(Items.COMPASS);
+    }
 }
